9379300fd4c4079289265c0c8da694ba0b64d4be
Vitor Angelozi <vitorangelozi2013@gmail.com>
2026-06-15 22:33:47 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import flask 
+from flask import request, jsonify
+app = flask.Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    # Process the incoming webhook data
+    return jsonify({"status": "success"})
